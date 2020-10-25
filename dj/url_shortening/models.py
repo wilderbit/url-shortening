9b4f190c64a27284d6ccb637732834a7837dc8da
AbrarNitk <abrar.nitk@gmail.com>
2020-10-25 20:04:52 +0530

    added postgres as databse and url models

--- dj/url_shortening/models.py
+++ dj/url_shortening/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    hash = models.CharField(unique=True, max_length=15)
+    alias = models.CharField(max_length=64, null=True, blank=True)
+    original_url = models.CharField(max_length=4096)
+    expired_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
